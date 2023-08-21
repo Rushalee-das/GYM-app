2e15d2dd3aa806edeb82084d3576d7096e03f0d9
Rushalee <rushaleedas@gmail.com>
2023-08-21 06:37:20 +0530

    all set delete working and styles added

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 export const WorkoutForm = () => {
+    const {dispatch} = useWorkoutsContext()
     const [title, setTitle] = useState('')
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
     const [error, setError] = useState(null)
+    const [emptyFields ,setEmptyFields] = useState([])
 
     const handleSubmit = async(e) =>
     {
@@ -23,6 +26,7 @@ export const WorkoutForm = () => {
         if(!response.ok)
         {
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
         if(response.ok)
         {
@@ -30,6 +34,8 @@ export const WorkoutForm = () => {
             setLoad('') 
             setReps('')   
             setError(null)
+            setEmptyFields([])
+            dispatch({type : 'CREATE_WORKOUT', payload : json})
             console.log("new workout added", json);
         }
     }
@@ -43,18 +49,21 @@ export const WorkoutForm = () => {
             type='text'
             onChange={(e) => setTitle(e.target.value)}
             value={title}
+            className={emptyFields.includes('title') ? 'error' : ''}
         />
         <label>Load (kg)</label>
         <input 
             type='number'
             onChange={(e) => setLoad(e.target.value)}
             value={load}
+            className={emptyFields.includes('load') ? 'error' : ''}
         />
         <label>Reps</label>
         <input 
             type='number'
             onChange={(e) => setReps(e.target.value)}
             value={reps}
+            className={emptyFields.includes('reps') ? 'error' : ''}
         />
 
         <button>Add Workout</button>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 import WorkoutsDetails from "../components/WorkoutsDetails"
 import { WorkoutForm } from "../components/WorkoutForm"
 
 const Home = () => {
-    const [workouts , setWorkouts] = useState(null)
+    const {workouts, dispatch} = useWorkoutsContext()
     useEffect(() => {
         const fetchWorkouts = async () => {
             const response = await fetch('/api/workout')
@@ -11,11 +12,11 @@ const Home = () => {
 
             if (response.ok)
             {
-                setWorkouts(json)
+                dispatch({type : 'SET_WORKOUTS', payload : json})
             }
         }
         fetchWorkouts()
-    }, [])
+    }, [dispatch])
   return (
     <div className="home">
         <div className="workouts">
